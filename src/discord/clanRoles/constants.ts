d7127ee28de8866e77bf6a281f106c4dc130f35e
Maksim Gladki <maks.falonse@gmail.com>
2026-05-31 15:42:51 +0300

    feat: limit clan name to 16 symbols

--- src/discord/clanRoles/constants.ts
+++ src/discord/clanRoles/constants.ts
@@ -4,5 +4,5 @@ export const CLAN_LEADER_META_PREFIX = "clan:ldr:";
 export const CLAN_MOD_LEADER_META_PREFIX = "clan:mod:ldr:";
 
 export const CLAN_NAME_MIN_LEN = 2;
-export const CLAN_NAME_MAX_LEN = 100;
+export const CLAN_NAME_MAX_LEN = 16;
 export const MAX_CLAN_LEADERS = 2;
